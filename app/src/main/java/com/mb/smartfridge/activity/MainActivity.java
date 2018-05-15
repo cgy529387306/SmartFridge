33719b235d8aca04e99d88c0c86609aa5d9222f1
cgy529387306 <cai_guiyou@163.com>
2018-05-15 18:49:08 +0800

    XXXXXXXXXXXXXXXXXXXXXX

--- app/src/main/java/com/mb/smartfridge/activity/MainActivity.java
+++ app/src/main/java/com/mb/smartfridge/activity/MainActivity.java
@@ -1,31 +1,45 @@
 package com.mb.smartfridge.activity;
 
+import android.graphics.drawable.AnimationDrawable;
+import android.media.Image;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.widget.DrawerLayout;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.mb.smartfridge.R;
+import com.mb.smartfridge.adapter.DeviceAdapter;
 import com.mb.smartfridge.adapter.DrawerLayoutAdapter;
+import com.mb.smartfridge.entity.DeviceEntity;
 import com.mb.smartfridge.entity.DrawerlayoutEntity;
 import com.mb.smartfridge.utils.NavigationHelper;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends BaseActivity implements View.OnClickListener {
     private ListView lvDrawerlayout;
+    private ListView lvDevice;
     private DrawerLayout drawerLayout;
     private RelativeLayout leftMenu;
     private DrawerLayoutAdapter drawerlayoutAdapter;
+    private DeviceAdapter mDeviceAdapter;
+    private ImageView ivSearch;
+    private TextView tvSearch;
+    private LinearLayout llyCancelback,llyAdddevice;
     private List<DrawerlayoutEntity> list = new ArrayList<>();
     private int img[] = new int[]{R.mipmap.ic_about,R.mipmap.ic_store,R.mipmap.ic_edit_password,R.mipmap.ic_logout};
     private String[] text = new String[]{"关于我们","线上商城","修改密码","退出登录"};
+    private List<DeviceEntity> mDeviceEntityList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +47,7 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
         setContentView(R.layout.activity_main);
         setTitle("选择设备");
         initDrawerLayout();
+        initDeviceList();
         initView();
     }
     private void setTitle(String title) {
@@ -54,11 +69,33 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
     }
 
     private void initView() {
+        ivSearch = (ImageView) findViewById(R.id.iv_search);
+        tvSearch = findViewById(R.id.tv_search);
+        final AnimationDrawable anim = (AnimationDrawable) ivSearch.getBackground();
+        anim.start();
+        final Handler handler = new Handler();
+        Runnable runnable ;
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                ivSearch.setVisibility(View.GONE);
+                tvSearch.setText("我的设备");
+                llyCancelback.setVisibility(View.GONE);
+                llyAdddevice.setVisibility(View.VISIBLE);
+            }
+        };
+        handler.postDelayed(runnable,2000);
+
         drawerLayout = findViewById(R.id.dl_content_main_menu);
         leftMenu = findViewById(R.id.ll_left_menu);
         lvDrawerlayout = findViewById(R.id.lv_drawerlayout);
+        lvDevice =findViewById(R.id.lv_device);
+        llyCancelback =findViewById(R.id.lly_cancelback);
+        llyAdddevice =findViewById(R.id.lly_adddevice);
         drawerlayoutAdapter = new DrawerLayoutAdapter(MainActivity.this,list);
         lvDrawerlayout.setAdapter(drawerlayoutAdapter);
+        mDeviceAdapter = new DeviceAdapter(MainActivity.this,mDeviceEntityList);
+        lvDevice.setAdapter(mDeviceAdapter);
         lvDrawerlayout.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
@@ -79,6 +116,11 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
     private void initDrawerLayout(){
         for (int i = 0;i < img.length;i++) {
             list.add(new DrawerlayoutEntity(img[i],text[i]));
+        }
+    }
+    private void initDeviceList(){
+        for (int i = 0;i < 3;i++) {
+            mDeviceEntityList.add(new DeviceEntity("智能车载冰箱","G3JJ4KK5I8-6FR5"));
         }
     }
 
