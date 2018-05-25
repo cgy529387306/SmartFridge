c13fa92c7ffef7685df2533538cec4b384f22c9e
caigy <529387306@qq.com>
2018-05-25 19:14:29 +0800

    blueManager

--- app/src/main/java/com/mb/smartfridge/activity/MainActivity.java
+++ app/src/main/java/com/mb/smartfridge/activity/MainActivity.java
@@ -1,14 +1,24 @@
 package com.mb.smartfridge.activity;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.drawable.AnimationDrawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Message;
 import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.AdapterView;
@@ -20,25 +30,40 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.avos.avoscloud.AVUser;
+import com.calypso.bluelib.bean.MessageBean;
+import com.calypso.bluelib.bean.SearchResult;
+import com.calypso.bluelib.listener.OnConnectListener;
+import com.calypso.bluelib.listener.OnReceiveMessageListener;
+import com.calypso.bluelib.listener.OnSearchDeviceListener;
+import com.calypso.bluelib.listener.OnSendMessageListener;
+import com.calypso.bluelib.manage.BlueManager;
+import com.calypso.bluelib.utils.TypeConversion;
+import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.mb.smartfridge.R;
 import com.mb.smartfridge.adapter.DeviceAdapter;
 import com.mb.smartfridge.adapter.DrawerLayoutAdapter;
 import com.mb.smartfridge.entity.DeviceEntity;
 import com.mb.smartfridge.entity.DrawerlayoutEntity;
+import com.mb.smartfridge.utils.CommonUtils;
 import com.mb.smartfridge.utils.DialogHelper;
 import com.mb.smartfridge.utils.NavigationHelper;
+import com.mb.smartfridge.utils.ProgressDialogHelper;
 import com.mb.smartfridge.utils.ToastHelper;
+import com.mb.smartfridge.views.DividerItemDecoration;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends BaseActivity implements View.OnClickListener {
+
+    public static final String TAG = "MainActivity";
+
     private ListView lvDrawerlayout;
-    private ListView lvDevice;
+    private RecyclerView lvDevice;
     private DrawerLayout drawerLayout;
     private RelativeLayout leftMenu;
     private DrawerLayoutAdapter drawerlayoutAdapter;
-    private DeviceAdapter mDeviceAdapter;
+    private DeviceAdapter deviceAdapter;
 
     private TextView tvName;
 
@@ -54,14 +79,43 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
     private String[] text = new String[]{"关于我们","线上商城","修改密码","退出登录"};
     private List<DeviceEntity> mDeviceEntityList = new ArrayList<>();
     private BluetoothAdapter bluetoothAdapter;
+    private List<SearchResult> deviceList;
+    private BlueManager blueManager;
+
+    private OnConnectListener onConnectListener;
+    private OnSendMessageListener onSendMessageListener;
+    private OnSearchDeviceListener onSearchDeviceListener;
+    private OnReceiveMessageListener onReceiveMessageListener;
+    @SuppressLint("HandlerLeak")
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            String message = msg.obj.toString();
+            showToast(message);
+            switch (msg.what) {
+                case 0:
+                    break;
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+            }
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setTitle("选择设备");
         initDrawerLayout();
-        initDeviceList();
         initView();
+        initListener();
+        initBlueManager();
     }
 
     private void setTitle(String title) {
@@ -83,9 +137,6 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
     }
 
     private void initView() {
-        if(!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
-            Toast.makeText(this,"不支持BLE", Toast.LENGTH_SHORT).show();
-        }
         tvName = findViewById(R.id.tv_name);
         tvName.setText(AVUser.getCurrentUser().getUsername());
 
@@ -102,8 +153,27 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
         llyNoDevice = findViewById(R.id.lly_noDevice);
         drawerlayoutAdapter = new DrawerLayoutAdapter(MainActivity.this,list);
         lvDrawerlayout.setAdapter(drawerlayoutAdapter);
-        mDeviceAdapter = new DeviceAdapter(MainActivity.this,mDeviceEntityList);
-        lvDevice.setAdapter(mDeviceAdapter);
+
+        lvDevice.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+        lvDevice.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL_LIST));
+        deviceList = new ArrayList<>();
+        deviceAdapter = new DeviceAdapter(R.layout.item_device,deviceList);
+        lvDevice.setAdapter(deviceAdapter);
+    }
+
+    private void initListener(){
+        tvAddDevice.setOnClickListener(this);
+        findViewById(R.id.tv_openBluetooth).setOnClickListener(this);
+        findViewById(R.id.tv_reSearch).setOnClickListener(this);
+        findViewById(R.id.tv_cancelBack).setOnClickListener(this);
+        deviceAdapter.setOnItemClickListener(new BaseQuickAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(BaseQuickAdapter adapter, View view, int position) {
+                String mac = deviceList.get(position).getAddress();
+                blueManager.connectDevice(mac);
+            }
+        });
+
         lvDrawerlayout.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
@@ -128,11 +198,159 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
                 }
             }
         });
-        tvAddDevice.setOnClickListener(this);
-        findViewById(R.id.tv_openBluetooth).setOnClickListener(this);
-        findViewById(R.id.tv_reSearch).setOnClickListener(this);
-        findViewById(R.id.tv_cancelBack).setOnClickListener(this);
+    }
 
+    /**
+     * 初始化蓝牙管理，设置监听
+     */
+    public void initBlueManager() {
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (ContextCompat.checkSelfPermission(MainActivity.this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 2);
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                        Manifest.permission.READ_CONTACTS)) {
+                    Toast.makeText(MainActivity.this, "shouldShowRequestPermissionRationale", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+        onSearchDeviceListener = new OnSearchDeviceListener() {
+            @Override
+            public void onStartDiscovery() {
+                sendMessage(0, "正在搜索设备..");
+                Log.d(TAG, "onStartDiscovery()");
+            }
+
+            @Override
+            public void onNewDeviceFound(BluetoothDevice device) {
+                Log.d(TAG, "new device: " + device.getName() + " " + device.getAddress());
+            }
+
+            @Override
+            public void onSearchCompleted(List<SearchResult> bondedList, List<SearchResult> newList) {
+                Log.d(TAG, "SearchCompleted: bondedList" + bondedList.toString());
+                Log.d(TAG, "SearchCompleted: newList" + newList.toString());
+                ivSearch.setVisibility(View.GONE);
+                tvSearch.setText("我的设备");
+                deviceList.clear();
+                deviceList.addAll(newList);
+                deviceAdapter.notifyDataSetChanged();
+                sendMessage(0, CommonUtils.isEmpty(deviceList)?"无可连接设备":"搜索完成,点击列表进行连接！");
+                llyNoDevice.setVisibility(CommonUtils.isEmpty(deviceList)?View.VISIBLE:View.GONE);
+                lvDevice.setVisibility(CommonUtils.isEmpty(deviceList)?View.GONE:View.VISIBLE);
+            }
+
+            @Override
+            public void onError(Exception e) {
+                sendMessage(0, "搜索失败");
+            }
+        };
+        onConnectListener = new OnConnectListener() {
+            @Override
+            public void onConnectStart() {
+                ProgressDialogHelper.showProgressDialog(MainActivity.this,"连接中...");
+                Log.i("blue", "onConnectStart");
+            }
+
+            @Override
+            public void onConnectting() {
+                Log.i("blue", "onConnectting");
+            }
+
+            @Override
+            public void onConnectFailed() {
+                ProgressDialogHelper.dismissProgressDialog();
+                sendMessage(0, "连接失败！");
+                Log.i("blue", "onConnectFailed");
+            }
+
+            @Override
+            public void onConectSuccess(String mac) {
+                ProgressDialogHelper.dismissProgressDialog();
+                sendMessage(4, "连接成功 MAC: " + mac);
+                Log.i("blue", "onConectSuccess");
+            }
+
+            @Override
+            public void onError(Exception e) {
+                ProgressDialogHelper.dismissProgressDialog();
+                sendMessage(0, "连接异常！");
+                Log.i("blue", "onError");
+            }
+        };
+        onSendMessageListener = new OnSendMessageListener() {
+            @Override
+            public void onSuccess(int status, String response) {
+                sendMessage(0, "发送成功！");
+                Log.i("blue", "send message is success ! ");
+            }
+
+            @Override
+            public void onConnectionLost(Exception e) {
+                sendMessage(0, "连接断开！");
+                Log.i("blue", "send message is onConnectionLost ! ");
+            }
+
+            @Override
+            public void onError(Exception e) {
+                sendMessage(0, "发送失败！");
+                Log.i("blue", "send message is onError ! ");
+            }
+        };
+        onReceiveMessageListener = new OnReceiveMessageListener() {
+
+
+            @Override
+            public void onProgressUpdate(String what, int progress) {
+                sendMessage(1, what);
+            }
+
+            @Override
+            public void onDetectDataUpdate(String what) {
+                sendMessage(3, what);
+            }
+
+            @Override
+            public void onDetectDataFinish() {
+                sendMessage(2, "接收完成！");
+                Log.i("blue", "receive message is onDetectDataFinish");
+            }
+
+            @Override
+            public void onNewLine(String s) {
+                sendMessage(3, s);
+            }
+
+            @Override
+            public void onConnectionLost(Exception e) {
+                sendMessage(0, "连接断开");
+                Log.i("blue", "receive message is onConnectionLost ! ");
+            }
+
+            @Override
+            public void onError(Exception e) {
+                Log.i("blue", "receive message is onError ! ");
+            }
+        };
+        blueManager = BlueManager.getInstance(getApplicationContext());
+        blueManager.setOnSearchDeviceListener(onSearchDeviceListener);
+        blueManager.setOnConnectListener(onConnectListener);
+        blueManager.setOnSendMessageListener(onSendMessageListener);
+        blueManager.setOnReceiveMessageListener(onReceiveMessageListener);
+        blueManager.requestEnableBt();
+    }
+
+    /**
+     * @param type    0 修改状态  1 更新进度  2 体检完成  3 体检数据进度
+     * @param context
+     */
+    public void sendMessage(int type, String context) {
+        if (handler != null) {
+            Message message = handler.obtainMessage();
+            message.what = type;
+            message.obj = context;
+            handler.sendMessage(message);
+        }
     }
 
     private void checkBluetooth() {
@@ -150,39 +368,12 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
     private void searchDevice(){
         ivSearch.setVisibility(View.VISIBLE);
         tvSearch.setText("搜索可用设备");
-        llyCancelBack.setVisibility(View.GONE);
-        AnimationDrawable anim = (AnimationDrawable) ivSearch.getBackground();
-        anim.start();
-        Handler handler = new Handler();
-        Runnable runnable ;
-        runnable = new Runnable() {
-            @Override
-            public void run() {
-                ivSearch.setVisibility(View.GONE);
-                tvSearch.setText("我的设备");
-                tvAddDevice.setVisibility(View.VISIBLE);
-            }
-        };
-        handler.postDelayed(runnable,2000);
-    }
-
-    private void addDevice(){
-        ivSearch.setVisibility(View.VISIBLE);
-        tvSearch.setText("搜索可用设备");
         llyCancelBack.setVisibility(View.VISIBLE);
         tvAddDevice.setVisibility(View.GONE);
         AnimationDrawable anim = (AnimationDrawable) ivSearch.getBackground();
         anim.start();
-        Handler handler = new Handler();
-        Runnable runnable ;
-        runnable = new Runnable() {
-            @Override
-            public void run() {
-                ivSearch.setVisibility(View.GONE);
-                tvSearch.setText("我的设备");
-            }
-        };
-        handler.postDelayed(runnable,2000);
+        blueManager.setReadVersion(false);
+        blueManager.searchDevices();
     }
 
     private void cancelSearch(){
@@ -193,13 +384,10 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
     }
 
     private void checkDevice() {
-        llyNoDevice.setVisibility(View.VISIBLE);
-        // 此时应该有网络请求 得到设备信息  之后再做判断
         if (mDeviceEntityList.size() > 0){
             ivSearch.setVisibility(View.GONE);
             tvSearch.setText("我的设备");
             llyCancelBack.setVisibility(View.VISIBLE);
-
         }else { //没有设备就自动搜索
             searchDevice();
         }
@@ -208,10 +396,6 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
     private void initDrawerLayout(){
         for (int i = 0;i < img.length;i++) {
             list.add(new DrawerlayoutEntity(img[i],text[i]));
-        }
-    }
-    private void initDeviceList(){
-        for (int i = 0;i < 3;i++) {
         }
     }
 
@@ -223,10 +407,10 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
                 startActivity(intent);
                 break;
             case R.id.tv_addDevice:  //  添加设备
-                addDevice();
+                searchDevice();
                 break;
             case R.id.tv_reSearch:  //  重新搜索
-                addDevice();
+                searchDevice();
                 break;
             case R.id.tv_cancelBack:  //  取消返回
                 cancelSearch();
@@ -245,6 +429,34 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
     @Override
     protected void onPause() {
         super.onPause();
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (blueManager != null) {
+            blueManager.close();
+            blueManager = null;
+        }
+        if (handler != null) {
+            handler.removeCallbacksAndMessages(null);
+            handler = null;
+        }
+
+    }
+
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == 2) {
+            if (permissions[0].equals(Manifest.permission.ACCESS_COARSE_LOCATION) && grantResults[0]
+                    == PackageManager.PERMISSION_GRANTED) {
+            } else {
+                if (!ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.
+                        permission.ACCESS_COARSE_LOCATION)) {
+                    return;
+                }
+            }
+        }
     }
 
     @Override
@@ -288,6 +500,18 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
                     }
 
                 }, R.string.dialog_negative, null);
+    }
+
+    private void getSn(){
+        MessageBean item = new MessageBean(TypeConversion.getDeviceVersion());
+        blueManager.setReadVersion(true);
+        blueManager.sendMessage(item, true);
+    }
+
+    private void send(){
+        blueManager.setReadVersion(false);
+        MessageBean item = new MessageBean(TypeConversion.startDetect());
+        blueManager.sendMessage(item, true);
     }
 
 }
--- app/src/main/java/com/mb/smartfridge/adapter/DeviceAdapter.java
+++ app/src/main/java/com/mb/smartfridge/adapter/DeviceAdapter.java
@@ -1,60 +1,26 @@
 package com.mb.smartfridge.adapter;
 
-import android.content.Context;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.BaseAdapter;
-import android.widget.TextView;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.Nullable;
 
+import com.calypso.bluelib.bean.SearchResult;
+import com.chad.library.adapter.base.BaseQuickAdapter;
+import com.chad.library.adapter.base.BaseViewHolder;
 import com.mb.smartfridge.R;
-import com.mb.smartfridge.entity.DeviceEntity;
 
-import java.util.ArrayList;
 import java.util.List;
 
-public class DeviceAdapter extends BaseAdapter {
-    private Context mContext;
-    private List<DeviceEntity> dataList = new ArrayList<>();
-    public DeviceAdapter(Context mContext , List<DeviceEntity> dataList) {
-        this.mContext = mContext;
-        this.dataList = dataList;
+public class DeviceAdapter extends BaseQuickAdapter<SearchResult, BaseViewHolder> {
+
+
+    public DeviceAdapter(@LayoutRes int layoutResId, @Nullable List<SearchResult> data) {
+        super(layoutResId, data);
     }
 
     @Override
-    public int getCount() {
-        return dataList.size();
+    protected void convert(BaseViewHolder helper, SearchResult item) {
+        helper.setText(R.id.tv_name, item.getName());
+        helper.setText(R.id.tv_model, item.getAddress());
     }
 
-    @Override
-    public String getItem(int position) {
-        return null;
-    }
-
-    @Override
-    public long getItemId(int position) {
-        return position;
-    }
-
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        ViewHolder holder = null;
-        if (convertView == null) {
-            holder = new ViewHolder();
-            convertView = LayoutInflater.from(mContext).inflate(R.layout.item_device, null);
-            holder.tvName = (TextView) convertView.findViewById(R.id.tv_name);
-            holder.tvModel = (TextView) convertView.findViewById(R.id.tv_model);
-            convertView.setTag(holder);
-        } else {
-            holder = (ViewHolder) convertView.getTag();
-        }
-        holder.tvName.setText(dataList.get(position).getName());
-        holder.tvModel.setText(dataList.get(position).getModel());
-        return convertView;
-    }
-
-    static class ViewHolder {
-        TextView tvName;
-        TextView tvModel;
-    }
 }
